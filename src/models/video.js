4e9b970edd63e47ef2c2900491c00ff8c16e5216
Juan Gabriel <juangabriel@pixeloak.com>
2017-08-23 15:49:07 -0500

    Added video model

---
+++ src/models/video.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+var VideoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  publisher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, { 
+  timestamps: true 
+})
+
+var Video = mongoose.model('Video', VideoSchema)
+
+export default Video
