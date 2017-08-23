51b048efa2befbf59833e9aa0f049df89c1343f8
Juan Gabriel <juangabriel@pixeloak.com>
2017-08-23 16:58:37 -0500

    Imported ulter to videoRoutes

--- src/routes/videos.js
+++ src/routes/videos.js
@@ -1,17 +1,22 @@
 import express from 'express'
+import multer from 'multer'
 
 const router = express.Router()
+
+const upload = multer({ dest: 'temp/' })
 
 /* Make route handlers */
 
 const getVideos = (req, res) => {}
 
-const postVideos = (req, res) => {}
+const postVideos = (req, res) => {
+  
+}
 
 /* Hook router to route handlers */
 
 router.get('/videos', getVideos)
 
-router.post('/videos', postVideos)
+router.post('/videos', upload.single('video'), postVideos)
 
 export default router
