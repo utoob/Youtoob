32b1fe708339048737fea81aaeb3ff3363c355cb
Juan Gabriel <juangabriel@pixeloak.com>
2017-08-24 16:09:55 -0500

    Added webpack and react frontend

---
+++ client/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>Hello I am App</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
