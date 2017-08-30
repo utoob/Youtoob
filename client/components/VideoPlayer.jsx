14646560b40bf38b02118deeff337d2fb614dd11
Juan Gabriel <juangabriel@pixeloak.com>
2017-08-30 17:15:22 -0500

    Code Cleanup

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
 
+const videoOptions = (url) => ({
+  autoplay: false,
+  controls: true,
+  sources: [{
+    src: url,
+    type: 'video/mp4'
+  }],
+  fluid: true
+})
+
 class VideoPlayer extends Component {
   componentDidMount() {
-    this.player = videojs(this.videoNode, this.props, this.onPlayerReady)
+    const videoUrl = this.props.videoUrl
+    this.player = videojs(this.videoNode, videoOptions(videoUrl), this.onPlayerReady)
   }
   componentWillUnmount() {
     if (this.player) {
