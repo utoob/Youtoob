c52d979ce5a5587a6af9b1a79288e08911537429
Juan Gabriel <juangabriel@pixeloak.com>
2017-09-07 11:56:08 -0500

    Added ViewCount component and tests

--- client/components/Watch.js
+++ client/components/Watch.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import * as api from '../utils/api'
 import VideoPlayer from './VideoPlayer'
 import VideoList from './VideoList'
+import ViewCount from './ViewCount'
 
 class Watch extends Component {
   constructor(props) {
@@ -28,13 +29,16 @@ class Watch extends Component {
     if (this.state.video) {
       const videoUrl = api.watchVideoUrl(this.state.video)
       const uploaderName = this.state.video.uploader && this.state.video.uploader.username || 'Anonymous'
+      const viewCount = this.state.video.viewCount
+
       return (
         <div className="o-grid o-grid--small-full o-grid--medium-full mt-1">
           <div className="o-grid__cell o-grid__cell--width-70">
             <VideoPlayer videoUrl={videoUrl} />
-            <h2>{this.state.video.title}</h2>
-            <p>Uploaded by: {uploaderName}</p>
-            <p>{this.state.video.description}</p>
+              <h2>{this.state.video.title}</h2>
+              <p>Uploaded by: {uploaderName}</p>
+              <p>{this.state.video.description}</p>
+              {/* <ViewCount viewCount={viewCount} /> */}
           </div>
           <div className="o-grid__cell">
             <h3>Recommended to Watch:</h3>
