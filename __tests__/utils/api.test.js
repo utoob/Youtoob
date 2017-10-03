85f2d52961493b49ef5734843839d1e4272b1b38
Juan Gabriel <juangabriel@pixeloak.com>
2017-10-03 14:34:20 -0500

    Fixed minor typos

--- __tests__/utils/api.test.js
+++ __tests__/utils/api.test.js
@@ -13,7 +13,13 @@ afterEach(() => {
   localStorage.clear()
 })
 
-/* The purpose of this prompt is to get you familiarized with mocking parts of 
+/* 
+ * Note:
+ * Testing the register function is provided to you for reference.
+ * You don't need to implement any function for this prompt.
+ * You just have to write the tests.
+ *
+ * The purpose of this prompt is to get you familiarized with mocking parts of 
  * our application that is a dependency of the function we are testing.
  *
  * It is important that we isolate the functionality we are testing by
@@ -29,7 +35,7 @@ afterEach(() => {
  *
  * To pass the prompt:
  *
- * Simulate successful login
+ * -- Simulate successful login --
  *
  * 1. Create a testUser object with _id, username and token attributes.
  * 2. nock is a testing utility library that will allow you to dictate 
@@ -40,24 +46,16 @@ afterEach(() => {
  *    a. loginEndpoint has been called.
  *    b. expect the resolved value of api.login function to equal testUser.
  *    c. get the item from the local storage and expect its value to testUser.
+ * 4. Make sure to return the promise as that is the way jest knows whether the
+ *    test had run completely.
  * 
- * Simulate failed login
+ * -- Simulate failed login --
  * 
  * Assert the following:
  * 1. loginEndpoint has been called.
  * 2. Error should be defined on catch block.
  * 3. `user` key in localStorage should not be defined.
- *
- * Note:
- * Testing the register function is provided to you for reference.
- * You don't need to implement the api.login function for this prompt.
  */
-
-test('simulate successful login', () => {
-})
-
-test('simulate failed login', () => {
-})
 
 test('simulate register function', () => {
   // Mock response to http://localhost:3000/api/register
@@ -65,10 +63,16 @@ test('simulate register function', () => {
     .post('/api/register')
     .reply(200, testUser)
 
-  api.register(testUser).then((newUser) => {
+  return api.register(testUser).then((newUser) => {
     registerEndpoint.done() // (1.✓) Request to the endpoint is made!
     expect(newUser).toEqual(testUser) // (2.✓) Resolve value is equal to testUser
     expect(localStorage.getItem('user')).toEqual(JSON.stringify(testUser)) // (3.✓) user is saved to localStorage
   })
+})
+
+test('simulate successful login', () => {
+})
+
+test('simulate failed login', () => {
 })
 
